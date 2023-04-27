eead760e955ae741ee96e97e3e8f2f113a47347c
islam0548 <131891933+islam0548@users.noreply.github.com>
2023-04-27 13:47:12 +0200

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,69 +1,90 @@
-import math
-import os
-import re
+import signal
+import sys
 import time
 
 import heroku3
-import requests
-import spamwatch as spam_watch
-from validators.url import url
 
-from .. import *
-from ..Config import Config
-from ..core.logger import logging
-from ..core.managers import edit_delete, edit_or_reply
-from ..core.session import sbb_b
-from ..helpers import *
-from ..helpers.utils import _format, _jmthonutils, install_pip, reply_id
-from ..sql_helper.globals import gvarstatus
+from .Config import Config
+from .core.logger import logging
+from .core.session import sbb_b, tgbot
+from .helpers.functions.converter import Convert
+from .helpers.functions.musictool import *
+from .helpers.utils.utils import runasync
+from .sql_helper.globals import addgvar, delgvar, gvarstatus
 
+__version__ = "3.2.0"
+__license__ = "GNU Affero General Public License v3.0"
+__author__ = "سورس الملوك <https://github.com/sourcesemsem/PYROGRAM>"
+__copyright__ = f" حقوق سورس  الملوك (C) 2020 - 2022  {__author__}"
+
+sbb_b.version = __version__
+sbb_b.tgbot.version = __version__
+LOGS = logging.getLogger("سورس الملوك")
 bot = sbb_b
-LOGS = logging.getLogger(__name__)
-USERID = sbb_b.uid if Config.OWNER_ID == 0 else Config.OWNER_ID
-ALIVE_NAME = Config.ALIVE_NAME
+tbot = tgbot
 
-Heroku = heroku3.from_key(Config.HEROKU_API_KEY)
-heroku_api = "https://api.heroku.com"
-HEROKU_APP_NAME = Config.HEROKU_APP_NAME
-HEROKU_API_KEY = Config.HEROKU_API_KEY
-
-thumb_image_path = os.path.join(Config.TMP_DOWNLOAD_DIRECTORY, "thumb_image.jpg")
-
-mention = f"[{Config.ALIVE_NAME}](tg://user?id={USERID})"
-hmention = f"<a href = tg://user?id={USERID}>{Config.ALIVE_NAME}</a>"
-
-PM_START = []
-PMMESSAGE_CACHE = {}
-PMMENU = "pmpermit_menu" not in Config.NO_LOAD
-
-TMP_DOWNLOAD_DIRECTORY = Config.TMP_DOWNLOAD_DIRECTORY
-
-if Config.SPAMWATCH_API:
-    token = Config.SPAMWATCH_API
-    spamwatch = spam_watch.Client(token)
-else:
-    spamwatch = None
+StartTime = time.time()
+jmthonversion = "7.2.0"
 
 
-if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
-    os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
+def close_connection(*_):
+    print("تم اغلاق الاتصال بالسورس الملوك")
+    runasync(sbb_b.disconnect())
+    sys.exit(143)
 
 
-if Config.THUMB_IMAGE is not None:
-    if check := url(Config.THUMB_IMAGE):
-        try:
-            with open(thumb_image_path, "wb") as f:
-                f.write(requests.get(Config.THUMB_IMAGE).content)
-        except Exception as e:
-            LOGS.info(str(e))
+signal.signal(signal.SIGTERM, close_connection)
 
+UPSTREAM_REPO_URL = Config.UPSTREAM_REPO
 
-def set_key(dictionary, key, value):
-    if key not in dictionary:
-        dictionary[key] = value
-    elif isinstance(dictionary[key], list):
-        if value in dictionary[key]:
-            return
-        dictionary[key].append(value)
+if Config.PRIVATE_GROUP_BOT_API_ID == 0:
+    if gvarstatus("PRIVATE_GROUP_BOT_API_ID") is None:
+        Config.BOTLOG = False
+        Config.BOTLOG_CHATID = "me"
     else:
-        dictionary[key] = [dictionary[key], value]
+        Config.BOTLOG_CHATID = int(gvarstatus("PRIVATE_GROUP_BOT_API_ID"))
+        Config.PRIVATE_GROUP_BOT_API_ID = int(gvarstatus("PRIVATE_GROUP_BOT_API_ID"))
+        Config.BOTLOG = True
+else:
+    if str(Config.PRIVATE_GROUP_BOT_API_ID)[0] != "-":
+        Config.BOTLOG_CHATID = int(f"-{str(Config.PRIVATE_GROUP_BOT_API_ID)}")
+    else:
+        Config.BOTLOG_CHATID = Config.PRIVATE_GROUP_BOT_API_ID
+    Config.BOTLOG = True
+
+if Config.PM_LOGGER_GROUP_ID == 0:
+    if gvarstatus("PM_LOGGER_GROUP_ID") is None:
+        Config.PM_LOGGER_GROUP_ID = -100
+    else:
+        Config.PM_LOGGER_GROUP_ID = int(gvarstatus("PM_LOGGER_GROUP_ID"))
+elif str(Config.PM_LOGGER_GROUP_ID)[0] != "-":
+    Config.PM_LOGGER_GROUP_ID = int(f"-{str(Config.PM_LOGGER_GROUP_ID)}")
+
+try:
+    if Config.HEROKU_API_KEY is not None or Config.HEROKU_APP_NAME is not None:
+        HEROKU_APP = heroku3.from_key(Config.HEROKU_API_KEY).apps()[
+            Config.HEROKU_APP_NAME
+        ]
+    else:
+        HEROKU_APP = None
+except Exception:
+    HEROKU_APP = None
+
+
+# تعريفات مهمة
+COUNT_MSG = 0
+USERS = {}
+COUNT_PM = {}
+LASTMSG = {}
+CMD_HELP = {}
+ISAFK = False
+AFKREASON = None
+CMD_LIST = {}
+SUDO_LIST = {}
+INT_PLUG = ""
+LOAD_PLUG = {}
+
+# متغيرات
+BOTLOG = Config.BOTLOG
+BOTLOG_CHATID = Config.BOTLOG_CHATID
+PM_LOGGER_GROUP_ID = Config.PM_LOGGER_GROUP_ID
